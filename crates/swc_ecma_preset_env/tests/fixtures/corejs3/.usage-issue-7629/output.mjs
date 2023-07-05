c5406b54d30ebc184872643bf0a5d3ae4950e639
HomyeeKing <HomyeeKing@gmail.com>
2023-07-05 10:17:54 +0800

    fix(corejs): symbol async dispose

---
+++ crates/swc_ecma_preset_env/tests/fixtures/corejs3/.usage-issue-7629/output.mjs
@@ -0,0 +1,5 @@
+import "core-js/modules/es.symbol.js";
+import "core-js/modules/esnext.symbol.description.js";
+import "core-js/modules/es.object.to-string.js";
+import "core-js/modules/es.symbol.async-dispose.js";
+Symbol.asyncDispose;
